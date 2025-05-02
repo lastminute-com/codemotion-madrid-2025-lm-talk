1ba7b3d57dbd154ee4a75e2cb0d3e882c835edba
Sam Campisi <samantha.campisi@lastminute.com>
2025-05-02 11:19:35 +0200

    Use withDependencies approach for the user feature

--- src/features/user/domain/di/provisions.ts
+++ src/features/user/domain/di/provisions.ts
@@ -3,6 +3,13 @@
  */
 import diContainer from "@/src/core/domain/di/diContainer";
 import keys from "@/src/features/user/domain/di/keys";
-import loadUser from "@/src/features/user/domain/usecase/loadUser";
+import withDependencies from "@/src/core/domain/di/withDependencies";
+import loadUserFactory from "@/src/features/user/domain/usecase/loadUserFactory";
+
+const loadUser = withDependencies(loadUserFactory, keys.profileStore);
+/**
+ * We need to define the interface of our use case for DI purpose.
+ */
+export type LoadUser = typeof loadUser;
 
 diContainer.set(keys.loadUser, loadUser);
